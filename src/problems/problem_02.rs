67f2b74cd3b9207a28230076d60109028ad7a5a9
codingmentalmodels <codingmentalmodels@gmail.com>
2022-12-05 15:28:47 -0500

    Solve problem 02 through 03a

--- src/problems/problem_02.rs
+++ src/problems/problem_02.rs
@@ -1,11 +1,113 @@
 use crate::input::input::InputParser;
 
-pub fn solve_problem_02a(input: Vec<String>) -> usize {
-    unimplemented!();
+fn solve_problem_02a(input: Vec<(String, String)>) -> u32 {
+    input.into_iter().map(|(lhs, rhs)| HandShape::score_round(
+        &HandShape::from_string_problem_02a(lhs),
+        &HandShape::from_string_problem_02a(rhs)
+    )).sum()
 }
 
-fn solve_problem_02b(input: Vec<String>) -> usize {
-    unimplemented!();
+fn solve_problem_02b(input: Vec<(String, String)>) -> u32 {
+    input.into_iter().map(|(lhs, rhs)| HandShape::score_round(
+        &Outcome::from_string(&lhs).get_hand_shape_to_achieve(&HandShape::from_string(rhs.clone())),
+        &HandShape::from_string(rhs)
+    )).sum()
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+enum HandShape {
+    Rock,
+    Paper,
+    Scissors
+}
+
+impl HandShape {
+
+    pub fn from_string(s: String) -> Self {
+        match s.as_str() {
+            "A" => Self::Rock,
+            "B" => Self::Paper,
+            "C" => Self::Scissors,
+            _ => panic!("Invalid hand shape: {}", s)
+        }
+    }
+
+    pub fn from_string_problem_02a(s: String) -> Self {
+        match s.as_str() {
+            "A" | "X" => Self::Rock,
+            "B" | "Y" => Self::Paper,
+            "C" | "Z" => Self::Scissors,
+            _ => panic!("Invalid hand shape: {}", s)
+        }
+    }
+
+    pub fn to_score(&self) -> u32 {
+        match self {
+            Self::Rock => 1,
+            Self::Paper => 2,
+            Self::Scissors => 3,
+        }
+    }
+
+    pub fn evaluate(lhs: &HandShape, rhs: &HandShape) -> Outcome {
+        if lhs == rhs { return Outcome::Draw; };
+
+        match (lhs, rhs) {
+            (Self::Rock, Self::Paper) => { Outcome::Lose },
+            (Self::Rock, Self::Scissors) => { Outcome::Win },
+            (Self::Paper, Self::Rock) => { Outcome::Win },
+            (Self::Paper, Self::Scissors) => { Outcome::Lose },
+            (Self::Scissors, Self::Rock) => { Outcome::Lose },
+            (Self::Scissors, Self::Paper) => { Outcome::Win },
+            _ => { panic!("It's a draw, but that shouldn't be possible.")}
+        }
+    }
+
+    pub fn score_round(lhs: &HandShape, rhs: &HandShape) -> u32 {
+        Self::evaluate(lhs, rhs).score() + lhs.to_score()
+    }
+
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+enum Outcome {
+    Win,
+    Draw,
+    Lose,
+}
+
+impl Outcome {
+
+    pub fn score(&self) -> u32 {
+        match self {
+            Self::Win => 6,
+            Self::Draw => 3,
+            Self::Lose => 0,
+        }
+    }
+
+    pub fn from_string(s: &str) -> Self {
+        match s {
+            "X" => Self::Lose,
+            "Y" => Self::Draw,
+            "Z" => Self::Win,
+            _ => panic!("Invalid outcome! {}", s)
+        }
+    }
+
+    pub fn get_hand_shape_to_achieve(&self, opponent_shape: &HandShape) -> HandShape {
+        if self == &Self::Draw { return opponent_shape.clone() };
+        match (opponent_shape, self) {
+            (&HandShape::Rock, Self::Win) => { HandShape::Paper },
+            (&HandShape::Rock, Self::Lose) => { HandShape::Scissors },
+            (&HandShape::Paper, Self::Win) => { HandShape::Scissors },
+            (&HandShape::Paper, Self::Lose) => { HandShape::Rock },
+            (&HandShape::Scissors, Self::Win) => { HandShape::Rock }, 
+            (&HandShape::Scissors, Self::Lose) => { HandShape::Paper }, 
+            (_, Self::Draw) => panic!("We needed to draw but didn't return above.")
+        }
+    }
+
 }
 
 #[cfg(test)]
@@ -16,24 +118,61 @@ mod test_problem_02 {
     #[test]
     fn test_problem_02a_passes() {
         
-        let input = InputParser::new().parse_as_string("input_02.txt").unwrap();
-        let shorted_input = input.iter().take(10).map(|i| i.clone()).collect();
+        let input: Vec<_> = InputParser::new().parse_as_string("input_02.txt").unwrap()
+            .into_iter().map(|x| x.chars().collect::<Vec<_>>()).map(|x| (x[2].to_string(), x[0].to_string())).collect();
+        
+        assert_eq!(input.len(), 2500);
 
-        assert_eq!(solve_problem_02a(shorted_input), 0);
+        let example_input = vec![
+            ("A".to_string(), "Y".to_string()),
+            ("B".to_string(), "X".to_string()),
+            ("C".to_string(), "Z".to_string()),
+        ];
+
+        assert_eq!(solve_problem_02a(example_input), 15);
+        // A Y => Y A => Paper vs. Rock => 6 + 2 = 8
+        // B Z => Z B => Scissors vs. Paper => 6 + 3 = 9
+        // C Y => Y C => Paper vs. Scissors => 0 + 2 = 2
+        // B Y => Y B => Paper vs. Paper => 3 + 2 = 5
+        // A Y => Y A => Paper vs. Rock => 6 + 2 = 8
+
+        // => 32
+
+        let shorted_input = input.iter().take(5).map(|i| i.clone()).collect();
+
+        assert_eq!(solve_problem_02a(shorted_input), 32);
 
         let answer = solve_problem_02a(input);
-        assert_eq!(answer, 0);
+
+        assert_eq!(answer, 15632);
     }
     
     #[test]
     fn test_problem_02b_passes() {
-        let input = InputParser::new().parse_as_string("input_02.txt").unwrap();
-        let shorted_input = input.iter().take(10).map(|i| i.clone()).collect();
+        
+        let input: Vec<_> = InputParser::new().parse_as_string("input_02.txt").unwrap()
+            .into_iter().map(|x| x.chars().collect::<Vec<_>>()).map(|x| (x[2].to_string(), x[0].to_string())).collect();
+        
+        // A Y => Y A => Draw vs. Rock => 3 + 1 = 4
+        // B Z => Z B => Win vs. Paper => 6 + 3 = 9
+        // C Y => Y C => Draw vs. Scissors => 3 + 3 = 6
+        // B Y => Y B => Draw vs. Paper => 3 + 2 = 5
+        // A Y => Y A => Draw vs. Rock => 3 + 1 = 4
 
-        assert_eq!(solve_problem_02b(shorted_input), 0);
+        // => 28
+
+        let shorted_input = input.iter().take(5).map(|i| i.clone()).collect();
+
+        assert_eq!(solve_problem_02b(shorted_input), 28);
 
         let answer = solve_problem_02b(input);
         assert_eq!(answer, 0);
     }
 
+    #[test]
+    fn test_can_compute_round_score() {
+        assert_eq!(HandShape::score_round(&HandShape::Rock, &HandShape::Rock), 4);
+        assert_eq!(HandShape::score_round(&HandShape::Scissors, &HandShape::Paper), 9);
+        assert_eq!(HandShape::score_round(&HandShape::Paper, &HandShape::Scissors), 2);
+    }
 }
--- src/problems/problem_03.rs
+++ src/problems/problem_03.rs
@@ -1,11 +1,39 @@
+use std::collections::HashSet;
+
 use crate::input::input::InputParser;
 
-pub fn solve_problem_03a(input: Vec<String>) -> usize {
+const AMOUNT_TO_SUBTRACT_FROM_LOWERCASE: u32 = 96;
+const AMOUNT_TO_SUBTRACT_FROM_UPPERCASE: u32 = 38;
+
+fn solve_problem_03a(input: Vec<String>) -> u32 {
+    input.into_iter().map(|rucksack| get_priority(get_common_element(rucksack))).sum()
+}
+
+fn solve_problem_03b(input: Vec<String>) -> u32 {
     unimplemented!();
 }
 
-fn solve_problem_03b(input: Vec<String>) -> usize {
-    unimplemented!();
+fn get_priority(c: char) -> u32 {
+    if c.is_lowercase() {
+        c as u32 - AMOUNT_TO_SUBTRACT_FROM_LOWERCASE
+    } else if c.is_uppercase() {
+        c as u32 - AMOUNT_TO_SUBTRACT_FROM_UPPERCASE
+    } else {
+        panic!("Input was neither lower nor uppercase: {}", c);
+    }
+}
+
+fn get_common_element(rucksack: String) -> char {
+    let length = rucksack.len();
+    assert_eq!(length % 2, 0);
+    let half_length = length / 2;
+    let rucksack_chars = rucksack.chars();
+    let first_half = rucksack_chars.clone().take(half_length).collect::<HashSet<_>>();
+    let second_half = rucksack_chars.skip(half_length).take(half_length).collect::<HashSet<_>>();
+    let intersection: Vec<_> = first_half.intersection(&second_half).collect();
+    assert_eq!(intersection.len(), 1);
+    
+    *intersection[0]
 }
 
 #[cfg(test)]
@@ -17,9 +45,25 @@ mod test_problem_03 {
     fn test_problem_03a_passes() {
         
         let input = InputParser::new().parse_as_string("input_03.txt").unwrap();
-        let shorted_input = input.iter().take(10).map(|i| i.clone()).collect();
+        
+        let example = vec![
+            "vJrwpWtwJgWrhcsFMMfFFhFp".to_string(),
+            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL".to_string(),
+            "PmmdzqPrVvPwwTWBwg".to_string(),
+            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn".to_string(),
+            "ttgJtRGJQctTZtZT".to_string(),
+            "CrZsJsPPZsGzwwsLwLmpwMDw".to_string(),
+        ];
+        assert_eq!(solve_problem_03a(example), 157);
+        
+        // let shortened_input = input.iter().take(3).map(|i| i.clone()).collect();
 
-        assert_eq!(solve_problem_03a(shorted_input), 0);
+        // // rNZNWvMZZmDDmwqNdZrWTqhJMhhgzggBhzBJBchQzzJJ => q => 17
+        // // pHlSVbVbFHgHBzzhQHqg => H => 26 + 8 = 34
+        // // nVsqGpbbtDtTNmrmfZ => t => 20
+
+        // // 17 + 34 + 20 = 71
+        // assert_eq!(solve_problem_03a(shortened_input), 71);
 
         let answer = solve_problem_03a(input);
         assert_eq!(answer, 0);
@@ -28,7 +72,7 @@ mod test_problem_03 {
     #[test]
     fn test_problem_03b_passes() {
         let input = InputParser::new().parse_as_string("input_03.txt").unwrap();
-        let shorted_input = input.iter().take(10).map(|i| i.clone()).collect();
+        let shorted_input = input.iter().take(3).map(|i| i.clone()).collect();
 
         assert_eq!(solve_problem_03b(shorted_input), 0);
 
@@ -36,4 +80,31 @@ mod test_problem_03 {
         assert_eq!(answer, 0);
     }
 
+    #[test]
+    fn test_letters_to_priority() {
+
+        assert_eq!('a' as u32 - AMOUNT_TO_SUBTRACT_FROM_LOWERCASE, 1);
+        assert_eq!('Z' as u32 - AMOUNT_TO_SUBTRACT_FROM_UPPERCASE, 52);
+
+        assert_eq!(get_priority('a'), 1);
+        assert_eq!(get_priority('b'), 2);
+        assert_eq!(get_priority('q'), 17);
+        assert_eq!(get_priority('H'), 34);
+        assert_eq!(get_priority('t'), 20);
+        assert_eq!(get_priority('z'), 26);
+        assert_eq!(get_priority('A'), 27);
+        assert_eq!(get_priority('Z'), 52);
+    }
+
+    #[test]
+    fn test_char_to_u32() {
+        assert_eq!('a' as u32, 97);
+        assert_eq!('b' as u32, 98);
+        assert_eq!('c' as u32, 99);
+        assert_eq!('z' as u32, 122);
+        assert_eq!('A' as u32, 65);
+        assert_eq!('B' as u32, 66);
+        assert_eq!('C' as u32, 67);
+        assert_eq!('Z' as u32, 90);
+    }
 }
